e0b78500d77c57a9304f77f3e2d00a3e54df9f03
Sing-T <nemo.singt@gmail.com>
2018-09-26 22:23:17 +0800

    Role BasicInfo Edit

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -7,6 +7,7 @@
           <span>角色管理</span>
         </div>
         <div>
+          <!--Form-->
           <el-form :inline="true" :model="form" class="demo-form-inline">
             <el-form-item label="角色名称">
               <el-input v-model="form.name" size="medium" placeholder="请输入角色名称"/>
@@ -24,9 +25,11 @@
         <el-card class="box-card">
           <el-row type="flex" justify="end" style="margin-bottom: 10px">
             <el-col :span="6" style="text-align: right">
-              <el-button v-waves class="filter-item" type="primary" size="small" icon="el-icon-plus">新增</el-button>
+              <!--Add-->
+              <el-button v-waves class="filter-item" type="primary" size="small" icon="el-icon-plus" @click="handleCreate">新增</el-button>
             </el-col>
           </el-row>
+          <!--Table-->
           <el-table v-loading="listLoading" :data="list" :key="key" border fit highlight-current-row style="width: 100%">
             <el-table-column :label="'角色名称'" align="center">
               <template slot-scope="scope">
@@ -55,12 +58,43 @@
               </template>
             </el-table-column>
           </el-table>
+          <!--Pagination-->
           <div class="pagination-container">
             <el-pagination :current-page="listQuery.page" :page-sizes="[10,20,30,50]" :page-size="listQuery.maxResultCount" :total="total" background layout="total, sizes, prev, pager, next, jumper" @size-change="handleSizeChange" @current-change="handleCurrentChange"/>
           </div>
         </el-card>
       </el-col>
     </el-row>
+
+    <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
+      <el-form
+        ref="dataForm"
+        :rules="rules"
+        :model="temp"
+        label-position="right"
+        label-width="80px"
+        style="margin-left:50px;margin-right: 60px;">
+        <el-tabs v-model="activeTab" type="border-card">
+          <el-tab-pane label="角色信息" name="roleInfo" style="padding-top: 10px">
+            <el-form-item :label="'角色名称'" prop="name">
+              <el-input v-model="temp.name" maxlength="32"/>
+            </el-form-item>
+            <el-form-item :label="'显示名称'" prop="displayName">
+              <el-input v-model="temp.displayName" maxlength="32"/>
+            </el-form-item>
+            <el-form-item :label="'角色描述'">
+              <el-input v-model="temp.description" maxlength="1024"/>
+            </el-form-item>
+          </el-tab-pane>
+          <el-tab-pane label="角色权限" name="rolePermission">配置管理</el-tab-pane>
+        </el-tabs>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取消</el-button>
+        <el-button v-if="dialogStatus=='create'" type="primary" @click="createData">确定</el-button>
+        <el-button v-else type="primary" @click="updateData">确定</el-button>
+      </div>
+    </el-dialog>
 
   </div>
 </template>
@@ -86,6 +120,7 @@ export default {
       form: {
         name: ''
       },
+      key: 1, // table key
       list: [],
       total: 0,
       listLoading: true,
@@ -95,7 +130,23 @@ export default {
         skipCount: 0,
         name: null
       },
-      key: 1 // table key
+      activeTab: '',
+      temp: {
+        id: undefined,
+        name: '',
+        displayName: '',
+        description: ''
+      },
+      dialogFormVisible: false,
+      dialogStatus: '',
+      textMap: {
+        update: '编辑',
+        create: '新增'
+      },
+      rules: {
+        name: [{ required: true, message: '角色名称是必填的', trigger: 'blur' }],
+        displayName: [{ required: true, message: '显示名称是必填的', trigger: 'blur' }]
+      }
     }
   },
   created() {
@@ -121,7 +172,28 @@ export default {
       this.listQuery.page = val
       this.getList()
     },
+    resetTab() {
+      this.activeTab = 'roleInfo'
+    },
+    resetTemp() {
+      this.temp = {
+        id: undefined,
+        name: '',
+        displayName: '',
+        description: ''
+      }
+    },
+    handleCreate() {
+      this.resetTab()
+      this.resetTemp()
+      this.dialogStatus = 'create'
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
     handleUpdate(row) {
+      this.resetTab()
       this.temp = Object.assign({}, row) // copy obj
       this.temp.timestamp = new Date(this.temp.timestamp)
       this.dialogStatus = 'update'
@@ -139,6 +211,48 @@ export default {
       })
       const index = this.list.indexOf(row)
       this.list.splice(index, 1)
+    },
+    createData() {
+      this.$refs['dataForm'].validate((valid) => {
+        if (valid) {
+          this.temp.id = parseInt(Math.random() * 100) + 1024 // mock a id
+          this.temp.author = 'vue-element-admin'
+          /* createArticle(this.temp).then(() => {
+            this.list.unshift(this.temp)
+            this.dialogFormVisible = false
+            this.$notify({
+              title: '成功',
+              message: '创建成功',
+              type: 'success',
+              duration: 2000
+            })
+          })*/
+        }
+      })
+    },
+    updateData() {
+      this.$refs['dataForm'].validate((valid) => {
+        if (valid) {
+          const tempData = Object.assign({}, this.temp)
+          tempData.timestamp = +new Date(tempData.timestamp) // change Thu Nov 30 2017 16:41:05 GMT+0800 (CST) to 1512031311464
+          /* updateArticle(tempData).then(() => {
+            for (const v of this.list) {
+              if (v.id === this.temp.id) {
+                const index = this.list.indexOf(v)
+                this.list.splice(index, 1, this.temp)
+                break
+              }
+            }
+            this.dialogFormVisible = false
+            this.$notify({
+              title: '成功',
+              message: '更新成功',
+              type: 'success',
+              duration: 2000
+            })
+          })*/
+        }
+      })
     }
   }
 }
